ebe41add7a0a7df2bc1d7dd2019fa22743404a06
kekesibence <bence.kekesi@gmail.com>
2021-11-09 16:58:42 +0100

    szoveg ellenorzo letrehozasa

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var textHibaUzenet;
+var betuHibaUzenet;
 
 var mintaSzovegInput;
 var betuMeretInput;
@@ -10,10 +12,18 @@ function init() {
     betuMeretInput = document.getElementById('betuMeret');
     betuSzinInput = document.getElementById('betuSzin');
     hatterSzinInput = document.getElementById('hatterSzin');
+    
     document.getElementById('elonezet').innerHTML = mintaSzovegInput.value;
     mintaSzovegInput.addEventListener("input", szovegValtozas); 
+    textHibaUzenet = document.getElementById("textHibaUzenet");
+    mintaSzovegInput.addEventListener("input", szovegEllenorzes); 
+    
+
     document.getElementById('elonezet').style.fontSize = betuMeretInput.value + "pt";
     betuMeretInput.addEventListener("input", betuMeretValtozas);
+    betuHibaUzenet = document.getElementById("betuHibaUzenet");
+    betuMeretInput.addEventListener("input", betuEllenorzes);
+
     betuSzinInput.addEventListener("input", betuSzinValtozas);
     hatterSzinInput.addEventListener("input", hatterSzinValtozas);
     document.getElementById('alaphelyzet').addEventListener("click", alaphelyzetbeAllitas);
@@ -37,9 +47,31 @@ function hatterSzinValtozas() {
 
 function alaphelyzetbeAllitas() {
     document.getElementById('mintaSzoveg').reset();
-
 }
 
+function szovegEllenorzes() {
+    var szoveg = mintaSzovegInput.value.length;
+    console.log(szoveg);    
+    if (szoveg != 0) {
+        console.log("nincs hiba");
+        textHibaUzenet.style.color = "#d3d3d3"
+    } else {
+        console.log("hiba");
+        textHibaUzenet.style.color = "#ff0000"
+    }
+}
+
+function betuEllenorzes() {
+    var szam = betuMeretInput.value;
+    console.log(szam);    
+    if (szam > 1) {
+        console.log("nincs hiba");
+        betuHibaUzenet.style.color = "#d3d3d3"
+    } else {
+        console.log("hiba");
+        betuHibaUzenet.style.color = "#ff0000"
+    }
+}
 document.addEventListener('DOMContentLoaded', init);
 
 
